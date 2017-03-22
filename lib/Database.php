f48d0785e4f5ecf0cf4b202b0ef0db9bea9aa5ee
FISI-LAB09-12 <FISI-LAB09-12>
2017-03-21 21:59:51 -0500

    seguimos con la conexion POO php

---
+++ lib/Database.php
@@ -0,0 +1,28 @@
+<? php
+    
+ class Database{
+ 	public $db;
+ 	protected	$resultado;
+ 	protected	$prep;
+ 	protected	$consulta;
+
+    
+ 	function __construct ( $dbhost, $dbuser, $dbpass, $dbname){
+        
+ 		$this->db= new mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+        
+ 		if($this->db->connect_errno ){
+            trigger_error("Falto la conexion con MySQL,Tipo de Error -> ({$this->db->connect_error})", E_USER_ERROR);
+ 		}
+ 		
+        // este metodo debe ir despues de hacer una creacion
+        $this->db->set_charset(DB_CHARSET);
+        
+ 	}
+    
+    public function getClientes(){
+        $this->resultado = $this->db->query("SELECT * from usuario");
+        return $this->resultado ->fetch_assoc();
+ }
+}
+
